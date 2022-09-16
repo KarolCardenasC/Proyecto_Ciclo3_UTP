ef8ba79ab9c1dc36d70a4a98c0303ef88cdcace8
Lichundead <vallejoricaurte@gmail.com>
2022-09-16 11:49:57 -0500

    visitas mvc

--- panoptico-java/src/main/java/com/panoptico/model/VisitasGuiadas.java
+++ panoptico-java/src/main/java/com/panoptico/model/VisitasGuiadas.java
@@ -3,14 +3,56 @@ package com.panoptico.model;
 import java.util.Date;
 
 public class VisitasGuiadas {
+    private int idVisitas;
     private String cedula;
+    private String nombre;
+    private String apellido;
     private Date fecha;
 
-    public String getCedulaVisitas() {
+    public VisitasGuiadas() {
+
+    }
+
+    public VisitasGuiadas(String cedula, String nombre, String apellido, Date fecha) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fecha = fecha;
+    }
+
+    public int getIdVisitas() {
+        return idVisitas;
+    }
+
+    public String getCedula() {
         return cedula;
     }
 
-    public Date getFechaVisitas() {
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public Date getFecha() {
         return fecha;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
     }
 }
