3f87b6135daabec173b839fa62d666a5ef6b0f3a
Lichundead <vallejoricaurte@gmail.com>
2022-09-19 20:56:32 -0500

    parameters sql

--- panoptico-java/panoptico-db.sql
+++ panoptico-java/panoptico-db.sql
@@ -2,6 +2,13 @@
 -- LA BASE DE DATOS YA HA SIDO HOSTEADA EN LA NUBE
 -- PRUEBAS REALIZADAS EN POSTMAN CON LOCALHOST:8080
 
+--MYSQL_ADDON_HOST=blamyq32slzamzmbtsev-mysql.services.clever-cloud.com
+--MYSQL_ADDON_DB=blamyq32slzamzmbtsev
+--MYSQL_ADDON_USER=uu6nxlbqcka5x1jo
+--MYSQL_ADDON_PORT=3306
+--MYSQL_ADDON_PASSWORD=vVUtHVKuIATlQ0NABdm2
+--MYSQL_ADDON_URI=mysql://uu6nxlbqcka5x1jo:vVUtHVKuIATlQ0NABdm2@blamyq32slzamzmbtsev-mysql.services.clever-cloud.com:3306/blamyq32slzamzmbtsev
+
 CREATE TABLE estudiantes_inscritos(
 	id_estudiante int PRIMARY KEY AUTO_INCREMENT NOT NULL, 
 	cedula VARCHAR(30) NOT NULL,
