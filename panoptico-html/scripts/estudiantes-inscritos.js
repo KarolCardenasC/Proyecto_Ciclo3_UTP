3a7ade6822832e7dd8be4c3b27cf8dfb62f13641
Caro-ag90 <cacunag@unicaragena.edu.co>
2022-09-23 09:46:38 -0500

    insertar la asistencia de estudiante inscrito consultado x cedula

--- panoptico-html/scripts/estudiantes-inscritos.js
+++ panoptico-html/scripts/estudiantes-inscritos.js
@@ -1,4 +1,5 @@
 const URL_API = "http://localhost:8080/estudiantes_inscritos";
+const URL_INSCRITOS = "http://localhost:8080/ingreso/estudiantes";
 
 function submit_search (evt) {
     evt.preventDefault()    
@@ -15,19 +16,33 @@ function submit_search (evt) {
     //console.log(existe)
     if (existe){
         const respuesta = await fetch(`${URL_API}/${cedula}`)
-    const estudiante = await respuesta.json()
-    console.log(estudiante)
-    document.getElementById("mensaje-busqueda").innerText = "¡BIENVENIDO(A) " +estudiante.nombre + "\n"
-    + "AL MUESO PANÓPTICO DE IBAGUÉ!"
+        const estudiante = await respuesta.json()
+        console.log(estudiante)
+        document.getElementById("mensaje-busqueda").innerText = "¡BIENVENIDO(A) " +estudiante.nombre + "\n"
+        + "AL MUESO PANÓPTICO DE IBAGUÉ!"
+        registrarIngreso(estudiante)
     }
     else{
     document.getElementById("mensaje-busqueda").innerText = "Usuario consultado CC:" + cedula+
     " NO es estudiante registrado" 
     //alert("Usuario consultado NO es estudiante registrado")
     }
-   // listarMascotas([mascota])
+   
   }
 
+  async function registrarIngreso(estudiante){
+    const url_registrar_ingreso = URL_INSCRITOS+"/"+estudiante.cedula
+     // enviar petición
+     const resp = await fetch(url_registrar_ingreso, {
+        method: 'POST'
+      })
+      
+      /* const text = await resp.text()
+      alert(text) */
+    
+
+  }
+ 
 
 
 
