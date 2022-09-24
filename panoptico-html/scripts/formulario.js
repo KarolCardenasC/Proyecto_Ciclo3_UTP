cafe9242f6e1df2769e8996174c27a918c18d206
Lichundead <vallejoricaurte@gmail.com>
2022-09-24 01:28:15 -0500

    ingresar datos a DB visitas_guiadas

--- panoptico-html/scripts/formulario.js
+++ panoptico-html/scripts/formulario.js
@@ -1,19 +1,62 @@
 const URL_API = "http://localhost:8080/visitas_guiadas"
-const URL_VISITAS = "http://localhost:8080/ingreso/visitantes"
 
-function submit_post(evt) {
-  evt.preventDefault()
-  const cedula = evt.target.cedula.value
-  const nombre = evt.target.cedula.value
-  const telefono = evt.target.cedula.value
-  const email = evt.target.cedula.value
-  // console.log(cedula)
-  POST(cedula, nombre, telefono, email)
+const UPDATE_FLAG = {
+  update: false,
+  idVisitas: null,
 }
 
-async function POST(cedula, nombre, telefono, email) {
-  // enviar petición
-  const respuesta_visitante = await fetch(
-    `${URL_API}/register/${(cedula, nombre, telefono, email)}`
-  )
+function get_data_form(evt) {
+  evt.preventDefault()
+  const form = evt.target
+  const visitas_guiadas = {
+    cedula: form.cedula.value,
+    nombre: form.nombre.value,
+    telefono: form.telefono.value,
+    email: form.email.value,
+  }
+  console.table({ visitas_guiadas })
+  if (UPDATE_FLAG.update) {
+    // añade el idVisitas al objeto visitas_guiadas
+    visitas_guiadas.idVisitas = UPDATE_FLAG.idVisitas
+    update(visitas_guiadas)
+  } else {
+    create(visitas_guiadas)
+  }
+
+  clear(form)
+}
+
+function clear(form) {
+  form.cedula.value = ""
+  form.nombre.value = ""
+  form.telefono.value = ""
+  form.email.value = ""
+}
+
+async function create(visitas_guiadas) {
+  // Enviar petición
+  const resp = await fetch(URL_API, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(visitas_guiadas),
+  })
+  const text = await resp.text()
+  alert(text)
+}
+
+function set_value_form(visitas_guiadas) {
+  document
+    .getElementByidVisitas("cedula")
+    .setAttribute("value", visitas_guiadas.nombre)
+  document
+    .getElementByidVisitas("nombre")
+    .setAttribute("value", visitas_guiadas.apellidVisitaso)
+  document
+    .getElementByidVisitas("telefono")
+    .setAttribute("value", visitas_guiadas.raza)
+  document
+    .getElementByidVisitas("email")
+    .setAttribute("value", visitas_guiadas.foto)
 }
