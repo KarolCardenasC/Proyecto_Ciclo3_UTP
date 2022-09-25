181079461c4f3bfdb512875d24d5049b1ebd3151
Karol Cardenas <vanessacc102@gmail.com>
2022-09-25 01:53:33 -0500

    add: Icono Pestaña todas las ventanas // Fix modal formulario

--- panoptico-html/scripts/formulario.js
+++ panoptico-html/scripts/formulario.js
@@ -2,7 +2,11 @@ const URL_API = "http://localhost:8080/visitas_guiadas/register"
 
 function get_data_form(evt) {
   // Indicar que no recarge página al enviar el formulario
-  evt.preventDefault()
+  evt.preventDefault()  
+
+  let modal_titulo =  document.getElementById("exampleModalLabel")
+  let  modal_body =  document.getElementById("mensaje-busqueda")
+
   const form = evt.target
   const visitas_guiadas = {
     cedula: form.cedula.value,
@@ -11,20 +15,24 @@ function get_data_form(evt) {
     email: form.email.value,
   }
   console.table({ visitas_guiadas })
-  if (cedula.value == "" || /^\s+$/.test(cedula)) {
-    alert("Error!! El campo nombre no puede estar vacio")
-  } else if (nombre.value == "" || /^\s+$/.test(nombre)) {
-    alert(
-      "Error!! El campo telefono no puede estar vacio y debe contener números"
-    )
-  } else if (telefono.value == "" || /^\s+$/.test(telefono)) {
-    alert(
-      "Error!! El campo telefono no puede estar vacio y debe contener números"
-    )
-  } else if (email.value == "" || /^\s+$/.test(email)) {
-    alert("Error!! El campo email no puede estar vacío")
+  if (cedula.value == "" || /^\s+$/.test(cedula.value)) {
+    modal_titulo.innerText = "Error"
+    modal_body.innerText =  "Error!! El campo cedula no puede estar vacio"
+
+  } else if (nombre.value == "" || /^\s+$/.test(nombre.value)) {
+    modal_titulo.innerText = "Error"
+    modal_body.innerText =  "Error!! El campo nombre no puede estar vacio y debe contener números"
+
+  } else if (telefono.value == "" || /^\s+$/.test(telefono.value)) {
+    modal_titulo.innerText = "Error"
+    modal_body.innerText =  "Error!! El campo telefono no puede estar vacio y debe contener números"
+
+  } else if (email.value == "" || /^\s+$/.test(email.value)) {
+    modal_titulo.innerText = "Error"
+    modal_body.innerText =  "Error!! El campo email no puede estar vacío"
+
   } else {
-    create(visitas_guiadas)
+    create(visitas_guiadas, modal_titulo, modal_body)
   }
 
   clear(form)
@@ -37,7 +45,7 @@ function clear(form) {
   form.email.value = ""
 }
 
-async function create(visitas_guiadas) {
+async function create(visitas_guiadas, modal_titulo, modal_body) {
   // Enviar petición
   const resp = await fetch(URL_API, {
     method: "POST",
@@ -46,13 +54,18 @@ async function create(visitas_guiadas) {
     },
     body: JSON.stringify(visitas_guiadas),
   })
-  const text = await resp.text()
-  alert(text)
-}
 
-function alert(visitas_guiadas) {
-  document.getElementById("mensaje-busqueda").innerText =
-    visitas_guiadas.nombre + " " + visitas_guiadas.cedula
+  const text = await resp.text()
+  const exitoso = "Visitante registrado con éxito" + "\n" + "Estudiante No inscrito ingresado a la tabla registro con éxito"
+  
+  if(text == exitoso){
+    modal_titulo.innerText = "Bienvenido"
+    modal_body.innerText =  `Se registro el Visitante con éxito y fue ingresado a la base de datos correctamente!!!`
+  }else{
+    modal_titulo.innerText = "Error"
+    modal_body.innerText =  `La cedula digitada ya se encuentra en la base de datos!!!`
+  }
+
 }
 
 function irInicio() {
